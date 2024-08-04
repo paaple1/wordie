cb4f9cdc2988f8b19beba3e4214915d5d744d709
paaple1 <78888148+paaple1@users.noreply.github.com>
2024-08-05 02:16:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 };
 
+// 静的ファイルを提供するための設定
 app.use(express.static('public'));
 
 app.get('/search', async (req, res) => {
